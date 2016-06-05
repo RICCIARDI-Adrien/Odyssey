8b4cc35adda9c0c4abcad7c5ffad88df1ca2184f
RICCIARDI-Adrien <adrien.ricciardi@hotmail.fr>
2016-06-05 17:02:57 +0200

    Added raspicam package.

---
+++ Raspberry_Pi/package/odyssey/odyssey-raspicam/src/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiCLI.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/RaspiCLI.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiCamControl.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/RaspiCamControl.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiPreview.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/RaspiPreview.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiStill.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/RaspiStill.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiTex.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/RaspiTex.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiTexUtil.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/RaspiTexUtil.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/gl_scenes/mirror.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/gl_scenes/mirror.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/gl_scenes/models.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/gl_scenes/models.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/gl_scenes/sobel.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/gl_scenes/sobel.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/gl_scenes/square.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/gl_scenes/square.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/gl_scenes/teapot.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/gl_scenes/teapot.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/gl_scenes/yuv.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/gl_scenes/yuv.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/tga.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspistill.dir/tga.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "host_applications/linux/libs/bcm_host/include"
+  "host_applications/linux/apps/raspicam"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Raspberry_Pi/package/odyssey/odyssey-raspicam/src/host_applications/linux/apps/raspicam/CMakeFiles/raspividyuv.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiCLI.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspividyuv.dir/RaspiCLI.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiCamControl.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspividyuv.dir/RaspiCamControl.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiPreview.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspividyuv.dir/RaspiPreview.o"
+  "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/RaspiVidYUV.c" "/home/ar/Bureau/userland/host_applications/linux/apps/raspicam/CMakeFiles/raspividyuv.dir/RaspiVidYUV.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "host_applications/linux/libs/bcm_host/include"
+  "host_applications/linux/apps/raspicam"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
